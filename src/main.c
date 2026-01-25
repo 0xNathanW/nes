ac190b51b52bf8a19ebe9df55d60b1c1021699a7
Nathan Warren <nathan.warren465@gmail.com>
2026-01-25 20:12:49 +0000

    set up main for testing purposes
    - sets up main to test cpu instructions by printing cpu state at each
    instruction executed

--- src/main.c
+++ src/main.c
@@ -1,29 +1,33 @@
-#include <SDL2/SDL.h>
 #include "cart.h"
+#include "cpu.h"
 #include "nes.h"
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#define NESTEST_INSTRUCTION_LIMIT 8991
 
-int main(int argc, char *argv[]) { 
-    Cartridge* cart = load_cart("test_roms/dk.nes");
-    if (!cart) {
-        printf("error: could not load cart\n");
-        return 1;
-    }
-    print_cart_info(cart);
-    
+int main(int argc, char* argv[]) {
+    const char* rom_path = (argc > 1) ? argv[1] : "../test_roms/nestest.nes";
+
     NES* nes = nes_create();
     if (!nes) {
         printf("error: could not create NES\n");
-        free_cart(cart);
         return 1;
     }
 
-    while (1) {
+    if (!nes_load_cartridge(nes, rom_path)) {
+        printf("error: could not load cartridge: %s\n", rom_path);
+        nes_destroy(nes);
+        return 1;
+    }
+
+    print_cart_info(nes->cartridge);
+    // For nestest automation mode, start at $C000
+    nes->cpu.regs.pc = 0xC000;
+
+    for (int i = 0; i < NESTEST_INSTRUCTION_LIMIT; i++) {
+        cpu_trace(&nes->cpu);
+        cpu_step(&nes->cpu);
     }
 
     nes_destroy(nes);
-    free_cart(cart);
-    return 0;     
+    return 0;
 }
